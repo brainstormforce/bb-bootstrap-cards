8191246e61bfa9246c0239769bd6e21509f06d2a
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2024-06-06 12:46:50 +0530

    escaping user input data

--- bb-bootstrap-cards-module/includes/frontend.php
+++ bb-bootstrap-cards-module/includes/frontend.php
@@ -18,12 +18,12 @@
 			$attrs   = $module->get_attributes();
 		?>
 		<?php if ( 'yes' == $settings->photo_hyperlink && 'link' == $settings->card_btn_type ) : ?>
-		<a href="<?php echo $settings->link_field; ?>" target="<?php echo $settings->link_target; ?>" itemprop="url">
+		<a href="<?php echo esc_url( $settings->link_field ); ?>" target="<?php echo esc_attr( $settings->link_target ); ?>" itemprop="url">
 		<?php endif; ?>
 		<?php if ( 'yes' == $settings->photo_hyperlink && 'button' == $settings->card_btn_type ) : ?>
-		<a href="<?php echo $settings->btn_link; ?>" target="<?php echo $settings->btn_link_target; ?>" itemprop="url">
+		<a href="<?php echo esc_url( $settings->btn_link ); ?>" target="<?php echo esc_attr( $settings->btn_link_target ); ?>" itemprop="url">
 		<?php endif; ?>
-		<img class="<?php echo $classes; ?>" src="<?php echo $src; ?>" alt="<?php echo $alt; ?>" itemprop="image" <?php echo $attrs; ?> />
+		<img class="<?php echo esc_attr( $classes ); ?>" src="<?php echo esc_url( $src ); ?>" alt="<?php echo esc_attr( $alt ); ?>" itemprop="image" <?php echo $attrs; ?> />
 		<?php if ( 'yes' == $settings->photo_hyperlink && 'none' != $settings->card_btn_type ) : ?>
 		</a>
 		<?php endif; ?>
